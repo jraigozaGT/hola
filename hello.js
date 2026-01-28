bdf53c51c962f55d76833ca66a43948b8b3c18b6
jraigozaGT <canserbero1108f@gmail.com>
2026-01-27 19:16:48 -0500

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,10 @@
+let hola = 30;
+
+function dias(){
+  hola += 1;
+  document.getElementById("texto").textContent = hola;
+}
+
+function color(){
+  document.getElementById("cuerpo").stylr.backgroundColor = purple;
+}
